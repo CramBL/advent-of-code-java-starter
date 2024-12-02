3d02393e7c67d1b1113978aa643f920fe2e14c4f
Marc Beck König <mbkj@tutamail.com>
2024-12-02 18:20:01 +0100

    day 2 part 1

--- src/main/java/aoc/day02/Day02.java
+++ src/main/java/aoc/day02/Day02.java
@@ -1,13 +1,57 @@
 package aoc.day02;
 
 import aoc.Day;
+import aoc.Utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day02 implements Day {
 
+    public static List<List<Integer>> inputToIntegerLists(String input) {
+        List<List<Integer>> reports = new ArrayList<>();
+        for (String line : input.split("\n")) {
+            List<Integer> nums = Arrays.stream(line.split(" "))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            reports.add(nums);
+        }
+        return reports;
+    }
+
+    public static Boolean isAllIncreasingOrDecreasing(List<Integer> report) {
+
+        boolean increasing = report.equals(report.stream().sorted().toList());
+        boolean decreasing = report.equals(report.stream().sorted(Comparator.reverseOrder()).toList());
+
+        return increasing || decreasing;
+    }
+
+    public static Boolean isLevelDistanceOnetoThree(List<Integer> report) {
+        for (int i = 0; i < report.size() - 1; i++) {
+            int difference = Math.abs(report.get(i) - report.get(i + 1));
+            if (difference < 1 || difference > 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Override
     public String part1(String input) {
+        var reports = inputToIntegerLists(input);
+        int safe_reports = 0;
+        for (var report : reports) {
+            if (isAllIncreasingOrDecreasing(report) && isLevelDistanceOnetoThree(report)) {
+                ++safe_reports;
+            }
+        }
 
-        return String.valueOf(1);
+        return String.valueOf(safe_reports);
     }
 
     @Override
--- src/test/java/aoc/day02/Day02Test.java
+++ src/test/java/aoc/day02/Day02Test.java
@@ -10,13 +10,19 @@ public class Day02Test {
     public void testPart1() {
         // Given
         String input = """
+                7 6 4 2 1
+                1 2 7 8 9
+                9 7 6 2 1
+                1 3 2 4 5
+                8 6 4 4 1
+                1 3 6 7 9
                 """;
 
         // When
         String result = new Day02().part1(input);
 
         // Then
-        assertEquals("1", result);
+        assertEquals("2", result);
     }
 
     @Test
