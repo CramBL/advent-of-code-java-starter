cd533b5c63f274ad30f031c3d7d078ac048d7cc7
Marc Beck König <mbkj@tutamail.com>
2024-12-02 17:50:18 +0100

    fix lints

--- src/test/java/aoc/day01/Day01Test.java
+++ src/test/java/aoc/day01/Day01Test.java
@@ -1,26 +1,22 @@
 package aoc.day01;
 
-
 import org.junit.jupiter.api.Test;
-
-import java.util.Collections;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day01Test {
 
     @Test
-    public void testPart1(){
+    public void testPart1() {
         // Given
         String input = """
-               3   4
-               4   3
-               2   5
-               1   3
-               3   9
-               3   3
-               """;
+                3   4
+                4   3
+                2   5
+                1   3
+                3   9
+                3   3
+                """;
 
         // When
         String result = new Day01().part1(input);
@@ -30,7 +26,7 @@ public class Day01Test {
     }
 
     @Test
-    public void testPart2(){
+    public void testPart2() {
         // Given
         String input = """
                 3   4
