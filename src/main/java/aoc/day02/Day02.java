93ec57c4fc9aaffbb4e1f333944e05ca2b832ebf
Marc Beck König <mbkj@tutamail.com>
2024-12-02 17:56:31 +0100

    add day02 boilerplate

---
+++ src/main/java/aoc/day02/Day02.java
@@ -0,0 +1,19 @@
+package aoc.day02;
+
+import aoc.Day;
+
+public class Day02 implements Day {
+
+    @Override
+    public String part1(String input) {
+
+        return String.valueOf(1);
+    }
+
+    @Override
+    public String part2(String input) {
+
+        return String.valueOf(2);
+    }
+
+}
---
+++ src/test/java/aoc/day02/Day02Test.java
@@ -0,0 +1,34 @@
+package aoc.day02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day02Test {
+
+    @Test
+    public void testPart1() {
+        // Given
+        String input = """
+                """;
+
+        // When
+        String result = new Day02().part1(input);
+
+        // Then
+        assertEquals("1", result);
+    }
+
+    @Test
+    public void testPart2() {
+        // Given
+        String input = """
+                """;
+
+        // When
+        String result = new Day02().part2(input);
+
+        // Then
+        assertEquals("2", result);
+    }
+}
