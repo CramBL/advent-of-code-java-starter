d295904d1d1d01bc8b4bff0d05771471f816ed26
Marc Beck König <mbkj@tutamail.com>
2024-12-03 16:42:37 +0100

    add day04 boiler plate while we're at it

---
+++ src/main/java/aoc/day04/Day04.java
@@ -0,0 +1,20 @@
+package aoc.day04;
+
+import aoc.Day;
+
+public class Day04 implements Day {
+
+    @Override
+    public String part1(String input) {
+
+        return String.valueOf(1);
+    }
+
+
+    @Override
+    public String part2(String input) {
+
+        return String.valueOf(2);
+    }
+
+}
