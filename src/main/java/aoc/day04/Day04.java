4f1100402a434abf75dbdd0800c306c46400d38e
Marc Beck König <mbkj@tutamail.com>
2024-12-04 12:23:17 +0100

    initial part 1 (I have 13 hours on a train tomorrow, so lets save some for that)

--- src/main/java/aoc/day04/Day04.java
+++ src/main/java/aoc/day04/Day04.java
@@ -2,10 +2,27 @@ package aoc.day04;
 
 import aoc.Day;
 
+import aoc.Utils;
+
 public class Day04 implements Day {
+
+    enum Sequence {
+        X,
+        M,
+        A,
+        S;
+    }
+
+    Boolean checkHorizontalForward(Sequence expect, String input) {
+        // switch expect
+
+        return true;
+    }
 
     @Override
     public String part1(String input) {
+
+        var lines = Utils.splitLines(input);
 
         return String.valueOf(18);
     }
