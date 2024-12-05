f083a034888e7202237634a761ce7d602c1002b4
Marc Beck König <mbkj@tutamail.com>
2024-12-05 09:42:35 +0100

    day06 boilerplate

---
+++ src/test/java/aoc/day06/Day06Test.java
@@ -0,0 +1,36 @@
+package aoc.day06;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Day06Test {
+
+    @Test
+    public void testPart1() {
+        // Given
+        String input =
+            """
+            """;
+
+        // When
+        String result = new Day06().part1(input);
+
+        // Then
+        assertEquals("1", result);
+    }
+
+    @Test
+    public void testPart2() {
+        // Given
+        String input =
+            """
+            """;
+
+        // When
+        String result = new Day06().part2(input);
+
+        // Then
+        assertEquals("2", result);
+    }
+}
