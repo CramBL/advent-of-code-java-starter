8dacefe334424ef4f9b893bdae12bf9bc5f7e881
Marc Beck König <mbkj@tutamail.com>
2024-12-09 17:17:31 +0100

    day 7 part 1

--- src/main/java/aoc/day07/Day07.java
+++ src/main/java/aoc/day07/Day07.java
@@ -1,12 +1,94 @@
 package aoc.day07;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import aoc.Day;
+import aoc.Utils;
 
 public class Day07 implements Day {
 
+    enum Operator {
+        Add {
+            @Override
+            Operator nextOp() {
+                return Mul;
+            }
+
+            @Override
+            Long evaluate(Long current_val, Long next_val) {
+                return current_val + next_val;
+            }
+        },
+        Mul {
+            @Override
+            Operator nextOp() {
+                return Add;
+            }
+
+            @Override
+            Long evaluate(Long current_val, Long next_val) {
+                return current_val * next_val;
+            }
+        };
+
+        abstract Operator nextOp();
+
+        abstract Long evaluate(Long current_val, Long next_val);
+    }
+
+    static boolean isSolvable(long testval, List<Integer> nums) {
+        return solve(testval, nums, 0, nums.get(0), Operator.Add) ||
+                solve(testval, nums, 0, nums.get(0), Operator.Mul);
+    }
+
+    static boolean solve(long testval, List<Integer> nums, int index, long current, Operator op) {
+        if (index == nums.size() - 1) {
+            return current == testval;
+        }
+        long next_val = nums.get(index + 1);
+
+        if (solve(testval, nums, index + 1,
+                op.evaluate(current, next_val),
+                Operator.Add)) {
+            return true;
+        }
+
+        return solve(testval, nums, index + 1,
+                op == Operator.Add ? Operator.Mul.evaluate(current, next_val)
+                        : Operator.Add.evaluate(current, next_val),
+                Operator.Mul);
+    }
+
+    static Long parseTestValue(String line) {
+        var equation_parts = line.split(":");
+        assert (equation_parts.length == 2);
+        Long test_value = Long.parseLong(equation_parts[0]);
+
+        var equation_nums = equation_parts[1].split(" ");
+        List<Integer> nums = new ArrayList<>();
+        for (var n : equation_nums) {
+            if (n.length() > 0) {
+                nums.add(Integer.parseInt(n));
+            }
+        }
+
+        if (isSolvable(test_value, nums)) {
+            return test_value;
+        }
+
+        return 0L;
+    }
+
     @Override
     public String part1(String input) {
-        return String.valueOf(1);
+        var lines = Utils.splitLines(input);
+        Long sum = 0L;
+        for (var l : lines) {
+            sum += parseTestValue(l);
+            System.out.println("-- " + sum + " --");
+        }
+        return String.valueOf(sum);
     }
 
     @Override
--- src/test/java/aoc/day07/Day07Test.java
+++ src/test/java/aoc/day07/Day07Test.java
@@ -6,9 +6,17 @@ import org.junit.jupiter.api.Test;
 
 public class Day07Test {
 
-    String input =
-        """
-        """;
+    String input = """
+            190: 10 19
+            3267: 81 40 27
+            83: 17 5
+            156: 15 6
+            7290: 6 8 6 15
+            161011: 16 10 13
+            192: 17 8 14
+            21037: 9 7 18 13
+            292: 11 6 16 20
+                    """;
 
     @Test
     public void testPart1() {
@@ -19,7 +27,7 @@ public class Day07Test {
         String result = new Day07().part1(input);
 
         // Then
-        assertEquals("1", result);
+        assertEquals("3749", result);
     }
 
     @Test
