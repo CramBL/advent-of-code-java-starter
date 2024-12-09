3e5d9a85914e926bb33b6a53fa0e7a590cf2162c
Marc Beck König <mbkj@tutamail.com>
2024-12-09 11:40:23 +0100

    make Pair compatible with HashSet

--- src/main/java/aoc/Pair.java
+++ src/main/java/aoc/Pair.java
@@ -1,5 +1,7 @@
 package aoc;
 
+import java.util.Objects;
+
 // Tuple Pair
 public class Pair<T, U> {
     private final T first;
@@ -16,5 +18,29 @@ public class Pair<T, U> {
 
     public U getSecond() {
         return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) &&
+                Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
     }
 }
